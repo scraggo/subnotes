363190246d0645c1e26bdc2b54bb98172d28f43b
Dave Cohen <dcohen18@gmail.com>
2017-11-12 18:01:35 -0600

    Add displayFilteredTags prototype with bugs

--- app/tests/filterTagsTest.js
+++ app/tests/filterTagsTest.js
@@ -16,7 +16,7 @@ function tag_filter(encList, f_tag) {
   // let f_tag = GETELEMENT!
   //ADD @ IF @ ISN'T FIRST
   if (!f_tag.startsWith('@')) {
-    f_tag = '@' + f_tag;
+    f_tag = '@' + f_tag.trim();
   }
   // let f_tag = input('Tag to search (include @): ')
 
@@ -26,8 +26,51 @@ function tag_filter(encList, f_tag) {
   // return this.encoded_list.filter( item => item.tags.indexOf(f_tag) > -1 );
 }
 
-function displayFilteredTags() {
+function displayFilteredTags(filteredList, f_tag) {
   // this.LOWEST_CHAR
+  let LOWEST_CHAR = '~'.repeat(10);
+  let tagArray = [];
+  let doneArray = [];
+  let i, item;
+  for (i = 0; i < filteredList.length; i++) {
+    item = filteredList[i];
+    // if (item.data.length < 1 || item.header === this.LOWEST_CHAR || item.header.length < 1) {
+    if ((item.header === LOWEST_CHAR || item.header.length < 1) && item.data.length < 1) {//REMEMBER TO REPLACE LOWEST_CHAR WITH this.LOWEST_CHAR IN THIS LINE
+      doneFiltered = item.done.filter( x => x.includes(f_tag));
+      doneArray.concat(doneFiltered);
+    } else {
+      headerSearch = item.header.includes(' ' + f_tag) ? item.header : false;
+      dataFiltered = item.data
+        .filter( x => x.includes(' ' + f_tag))
+        .map( x => x.trim() );
+      tagArray.push({headerTags: headerSearch, dataTags: dataFiltered });
+    }
+  }
+  console.log(tagArray, doneArray);
+
+  if (doneArray.length < 1 && tagArray.length < 1) {
+    return '<h2>No Tagged Items Found.</h2>'
+  }
+
+  // convert tagArray to array of strings
+  let tagArray2 = tagArray.map(function(item) {
+    if (item.headerTags !== false && item.dataTags !== false) {
+      return '' + item.headerTags + item.dataTags.join('\n') + '\n';
+    } else if (item.headerTags !== false && item.dataTags.length < 1) {
+      return '' + item.headerTags + '\n';
+    } else if (item.headerTags === false) {
+      return '' + item.dataTags.join('\n') + '\n';
+    }
+  })
+  
+  console.log(tagArray2);
+  let doneString = '';
+  let doneHeader = '<h2>Done Items with Tag:</h2>';
+  if (doneArray.length > 0) {
+    doneString = doneHeader + '\n' + doneArray.join('\n');
+  }
+  let taggedHeader = '<h2>Tagged Items:</h2>';
+  return taggedHeader + '\n\n' + tagArray2.join('\n') + '\n' + doneString;
   // OR block:
   // if data = empty, header = lowest char, header = empty
   // append to 'done' at end for display
