3fc95893fefaed5a38ed89efc69706c78d2ec3b4
Dave Cohen <dcohen18@gmail.com>
2017-11-13 21:44:43 -0600

    Fix bug: now 'done' items are displaying with tags search

--- app/tests/filterTagsTest.js
+++ app/tests/filterTagsTest.js
@@ -13,7 +13,7 @@ function tag_filter(encList, f_tag) {
   let all_tags = ["@!", "@!!!", "@crazy", "@innertag", "@othertag", "@tag"];
 
   //get user input
-  // let f_tag = GETELEMENT!
+  // let f_tag = GET-ELEMENT!
   //ADD @ IF @ ISN'T FIRST
   if (!f_tag.startsWith('@')) {
     f_tag = '@' + f_tag.trim();
@@ -27,80 +27,60 @@ function tag_filter(encList, f_tag) {
 }
 
 function displayFilteredTags(filteredList, f_tag) {
+  let nothingFoundMessage = '<h2>No Tagged Items Found.</h2>';
+  if (filteredList.length < 1) {
+    return nothingFoundMessage;
+  }
   // this.LOWEST_CHAR
   let LOWEST_CHAR = '~'.repeat(10);
   let tagArray = [];
   let doneArray = [];
-  let i, item;
+  let i, item, doneFiltered, headerSearch, dataFiltered;
   for (i = 0; i < filteredList.length; i++) {
     item = filteredList[i];
     // if (item.data.length < 1 || item.header === this.LOWEST_CHAR || item.header.length < 1) {
-    if ((item.header === LOWEST_CHAR || item.header.length < 1) && item.data.length < 1) {//REMEMBER TO REPLACE LOWEST_CHAR WITH this.LOWEST_CHAR IN THIS LINE
-      doneFiltered = item.done.filter( x => x.includes(f_tag));
-      doneArray.concat(doneFiltered);
+    if (item.done.length > 0) {//REMEMBER TO REPLACE LOWEST_CHAR WITH this.LOWEST_CHAR IN THIS LINE
+      doneFiltered = item.done
+        .filter( x => x.includes(f_tag))
+        .map( x => x.trim() );
+      if (doneFiltered.length > 0) {
+        doneArray = doneArray.concat(doneFiltered);
+      }
+      // console.error(item.header, doneFiltered, doneArray);//debug
     } else {
       headerSearch = item.header.includes(' ' + f_tag) ? item.header : false;
       dataFiltered = item.data
         .filter( x => x.includes(' ' + f_tag))
         .map( x => x.trim() );
+      if (dataFiltered.length < 1) {
+        dataFiltered = false;
+      }
       tagArray.push({headerTags: headerSearch, dataTags: dataFiltered });
     }
   }
-  console.log(tagArray, doneArray);
+  // console.log(tagArray, doneArray);//debug
 
   if (doneArray.length < 1 && tagArray.length < 1) {
-    return '<h2>No Tagged Items Found.</h2>'
+    return nothingFoundMessage;
   }
 
   // convert tagArray to array of strings
   let tagArray2 = tagArray.map(function(item) {
     if (item.headerTags !== false && item.dataTags !== false) {
-      return '' + item.headerTags + item.dataTags.join('\n') + '\n';
-    } else if (item.headerTags !== false && item.dataTags.length < 1) {
-      return '' + item.headerTags + '\n';
+      return item.headerTags + item.dataTags.join('\n') + '\n';
+    } else if (item.headerTags !== false && item.dataTags === false) {
+      return item.headerTags + '\n';
     } else if (item.headerTags === false) {
-      return '' + item.dataTags.join('\n') + '\n';
+      return item.dataTags.join('\n') + '\n';
     }
   })
   
-  console.log(tagArray2);
+  // console.log(tagArray2);//debug
   let doneString = '';
   let doneHeader = '<h2>Done Items with Tag:</h2>';
   if (doneArray.length > 0) {
     doneString = doneHeader + '\n' + doneArray.join('\n');
   }
   let taggedHeader = '<h2>Tagged Items:</h2>';
-  return taggedHeader + '\n\n' + tagArray2.join('\n') + '\n' + doneString;
-  // OR block:
-  // if data = empty, header = lowest char, header = empty
-  // append to 'done' at end for display
-  // else
-  // display header AND data that contains tag.
+  return taggedHeader + '\n' + tagArray2.join('\n') + '\n' + doneString;
 }
-/*
-  if len(filtered_tags) > 0
-    print()
-  print('*' * (20 + len(f_tag)))
-  print('Projects with tag: {}'.format(f_tag))
-  print('*' * (20 + len(f_tag)))
-  for header_tag in filtered_tags
-    print(header_tag)
-  print()
-  else 
-    print('No notes with {} tag found.\n'.format(f_tag))
-  }
-
-}
-
-function view_all_tags(all_tags) {
-  // input?
-  let output = [];
-  if (all_tags.length < 1) {
-      alert('No tags found.\n');
-      return;
-  } else {
-      return 'Your Tags: ' + all_tags.join(', ');
-  }
-}
-
-*/
