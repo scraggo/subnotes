c5716cf745ed30ddfa9c6b49d5116dcbad69f939
Dave Cohen <dcohen18@gmail.com>
2017-11-15 11:48:00 -0600

    Add tag_filter and displayFilteredTags - untested

--- app/js/subnotes.js
+++ app/js/subnotes.js
@@ -209,6 +209,85 @@ class Subnotes {
 
     return Array.from(this.allTags).sort();
   }
+
+  tag_filter(f_tag) {
+    /*
+        Args:
+            f_tag: tag that user is searching for
+        Returns:
+            Array of data items with f_tag in item.tags
+        */
+
+    let all_tags = this.return_all_tags();
+
+    //get user input
+    // let f_tag = GET-ELEMENT!
+    //ADD @ IF @ ISN'T FIRST
+    if (!f_tag.startsWith('@')) {
+      f_tag = '@' + f_tag.trim();
+    }
+
+    //filter by f_tag
+    return this.encoded_list.filter( item => item.tags.indexOf(f_tag) > -1 );
+  }
+
+  displayFilteredTags(filteredList, f_tag) {
+    let nothingFoundMessage = '<h2>No Tagged Items Found.</h2>';
+    if (filteredList.length < 1) {
+      return nothingFoundMessage;
+    }
+    let tagArray = [];
+    let doneArray = [];
+    let i, item, doneFiltered, headerSearch, dataFiltered;
+    for (i = 0; i < filteredList.length; i++) {
+      item = filteredList[i];
+      //REMEMBER TO REPLACE LOWEST_CHAR WITH this.LOWEST_CHAR IN THIS LINE
+      // if (item.data.length < 1 || item.header === this.LOWEST_CHAR || item.header.length < 1) {
+      if (item.done.length > 0) {
+        doneFiltered = item.done
+          .filter( x => x.includes(f_tag))
+          .map( x => x.trim() );
+        if (doneFiltered.length > 0) {
+          doneArray = doneArray.concat(doneFiltered);
+        }
+        // console.error(item.header, doneFiltered, doneArray);//debug
+      } else {
+        headerSearch = item.header.includes(' ' + f_tag) ? item.header : false;
+        dataFiltered = item.data
+          .filter( x => x.includes(' ' + f_tag))
+          .map( x => x.trim() );
+        if (dataFiltered.length < 1) {
+          dataFiltered = false;
+        }
+        tagArray.push({headerTags: headerSearch, dataTags: dataFiltered });
+      }
+    }
+    // console.log(tagArray, doneArray);//debug
+
+    if (doneArray.length < 1 && tagArray.length < 1) {
+      return nothingFoundMessage;
+    }
+
+    // convert tagArray to array of strings
+    let tagArray2 = tagArray.map(function(item) {
+      if (item.headerTags !== false && item.dataTags !== false) {
+        return item.headerTags + item.dataTags.join('\n') + '\n';
+      } else if (item.headerTags !== false && item.dataTags === false) {
+        return item.headerTags + '\n';
+      } else if (item.headerTags === false) {
+        return item.dataTags.join('\n') + '\n';
+      }
+    })
+    
+    // console.log(tagArray2);//debug
+    let doneString = '';
+    let doneHeader = '<h2>Done Items with Tag:</h2>';
+    if (doneArray.length > 0) {
+      doneString = doneHeader + '\n' + doneArray.join('\n');
+    }
+    let taggedHeader = '<h2>Tagged Items:</h2>';
+    return taggedHeader + '\n' + tagArray2.join('\n') + '\n' + doneString;
+  }
 }
 
 // module.exports = new Subnotes;
